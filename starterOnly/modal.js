027eb8acde4914eaf5481087ef0aeb9a2a61fd8f
Marie Baude <baudemarie95@gmail.com>
2021-04-12 07:27:24 +0200

    premier essai requet 1, fermeture pop up

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,34 @@
+/** VARIABLES
+ * ---------------------------------------
+ * ---------------------------------------
+ */
+/** DOM elements */
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
+
+/** EVENTS
+ * ---------------------------------------
+ * ---------------------------------------
+ */
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+closeBtn.addEventListener("click", closeModal);
+
+/** FUNCTIONS
+ * ---------------------------------------
+ * ---------------------------------------
+ */
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,18 +37,3 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-
