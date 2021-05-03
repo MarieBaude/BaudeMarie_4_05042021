1f2522ddd91dc592478623f90b7d6bcd172d759a
Marie Baude <baudemarie95@gmail.com>
2021-05-03 11:44:43 +0200

    validation prenom et nom

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,75 +1,24 @@
-/** VARIABLES
- * ---------------------------------------
- * ---------------------------------------
- */
-let form = document.getElementsByTagName('form')[0];
-let fields = ['firstName', 'lastName', 'email', 'birthdate', 'quantity', 'location', 'checkbox'];
-let messages = {
-    firstName: "Le prénom n'est pas valide.",
-    lastName: "Le nom n'est pas valide.",
-    email: "L'email n'est pas valide.",
-    birthdate: "La date de naissance n'est pas valide.",
-	quantity: "La quantitée n'est pas valide.",
-	location: "Choisissez une ville.",
-	checkbox: "Merci d'accepter les conditions d'utilisations.",
-}
+/* VARIABLES 
+-----------------------------
+*/
+const form = document.getElementsByTagName('form')[0];
+const firstNameImput = document.getElementById('firstName');
+const lastNameImput = document.getElementById('lastName');
+const emailImput = document.getElementById('email');
+const birthdateImput = document.getElementById('birthdate');
+const quantityImput = document.getElementById('quantity');
 
-/** EVENTS
- * ---------------------------------------
- * ---------------------------------------
- */
+const fields = ['firstName', 'lastName', 'email'];
+
+/* EVENEMENTS 
+-----------------------------
+*/
 disableSubmitBtn();
 listenForFormChange();
-listenForEachFieldChange();
 
-/** FUNCTIONS
- * ---------------------------------------
- * ---------------------------------------
- */
-// Rétablir le bouton quand le formulaire est valide
-function listenForFormChange() {
-    form.addEventListener('keydown', function() {
-        disableSubmitBtn();
-        if (isFormValid()) {
-            enableSubmitBtn();
-        }
-    });
-}
-
-function isFormValid() {
-    let isValid = true;
-    fields.forEach(field => {
-        let functionName = 'is' + capitalize(field) + 'Valid';
-        let value = document.getElementById(field);
-        if (!window[functionName](value)) {
-            isValid = false;
-        }
-    })
-    return isValid;
-}
-
-function listenForEachFieldChange() {
-    fields.forEach(field => {
-        let input = document.getElementById(field);
-        input.addEventListener('keydown', function() {
-            hideError(field);
-            let functionName = 'is' + capitalize(field) + 'Valid';
-            if (!window[functionName](input.value)) {
-                showError(field, messages[field]);
-            }
-        })
-    })
-}
-
-function validate() {
-    if (!isNameValid(firstName.value)){
-        return false
-    }
-    if (!isNameValid(lastName.value)){
-        return false
-    } 
-}
-
+/* FONCTIONS 
+-----------------------------
+*/
 function disableSubmitBtn() {
     let submitBtn = document.querySelector('.btn-submit');
     submitBtn.setAttribute('disabled', 'disabled');
@@ -84,29 +33,51 @@ function enableSubmitBtn() {
     submitBtn.style.cursor = "pointer";
 }
 
-function showError(field, message) {
-    if (isFormValid) {
-        
-    }
-}
-
-function hideError(field) {
-    // valeur correct, retirer
-}
-
-//Majuscule
-function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-
-function isNameValid(name){
+function isFirstNameValid(name) {
     name = name.trim(' ');
-    if (name.length < 2) {
-        return false;
-    }
-    if (name.length > 20) {
+    if (name.length < 1) {
         return false;
     }
     return true;
+}
+
+function isLastNameValid(name) {
+    if (name.length < 1) {
+        return false;
+    }
+    return true;
+}
+
+function listenForFormChange() {
+    fields.forEach(field => {
+        let input = document.getElementById(field);
+        input.addEventListener('keydown', function () {
+            hideError(field);
+            if (!isFirstNameValid(field.value)) {
+                showError(field);
+            }
+            if (isFormValid()) {
+                enableSubmitBtn();
+            }
+        });
+    })
+}
+
+function isFormValid() {
+    let isValid = true;
+    if (isFirstNameValid(firstNameImput.value) &&
+        isLastNameValid(firstNameImput.value)) { 
+        isValid = false;
+    }
+    return isValid;
+}
+
+function showError(field) {
+    let parent = field.closest('div');
+    parent.setAttribute('data-error-visible', true)
+}
+
+function hideError(field) {
+    let parent = field.closest('div');
+    parent.setAttribute('date-error-visible', false);
 }
