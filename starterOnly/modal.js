667b4985a9af1a661b67ca57f8af835450fa33dd
Marie Baude <baudemarie95@gmail.com>
2021-05-17 10:29:11 +0200

    test ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,22 @@
-/** VARIABLES
- * ---------------------------------------
- * ---------------------------------------
- */
+/* VARIABLES 
+-----------------------------
+*/
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 
-/** EVENTS
- * ---------------------------------------
- * ---------------------------------------
- */
+
+/* EVENTS 
+-----------------------------
+*/
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.addEventListener("click", closeModal);
 
-/** FUNCTIONS
- * ---------------------------------------
- * ---------------------------------------
- */
+
+/* FUNCTIONS 
+-----------------------------
+*/
 function launchModal() {
   modalbg.style.display = "block";
 }
